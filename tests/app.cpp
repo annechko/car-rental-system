c06bb7ba7c5cf5f9e2d68c604dc8b302189636f4
Anna Borzenko <annnechko@gmail.com>
2024-01-16 14:02:22 +1300

    tests, refactor for readability

--- tests/app.cpp
+++ tests/app.cpp
@@ -8,6 +8,28 @@
 #include <list>
 #include <console/application.h>
 
+std::vector<char*> parse_argv(const std::string& cmnd)
+{
+    using namespace std;
+
+    istringstream command_stream(cmnd);
+    string s;
+    std::vector<char*> opts_car_add;
+
+    while (getline(command_stream, s, ' '))
+    {
+        char* val = new char[s.size()];
+        copy(s.begin(), s.end(), val);
+        opts_car_add.push_back(val);
+    }
+    return opts_car_add;
+};
+
+const int parse_argc(const std::string& s)
+{
+    return std::count(s.begin(), s.end(), ' ') + 1;
+}
+
 /* UNIT */
 void test_create_user()
 {
@@ -17,14 +39,19 @@ void test_create_user()
     assert_equals(user.get_role(), crs::core::user::USER_ROLE::CUSTOMER, __FUNCTION__);
 }
 
+std::stringstream run_app(const std::string command)
+{
+    std::stringstream buffer;
+    auto argv = parse_argv(command);
+    auto app = new crs::console::application(parse_argc(command), &argv[0], buffer);
+    app->handle();
+    return buffer;
+}
+
 /* ACCEPTANCE */
 void test_help_when_no_command_expect_see_all_commands_helps()
 {
-    char* argv_test[] = { (char*)"car_rental_system", (char*)"-h", };
-    int argc_test = 2;
-    std::stringstream buffer;
-    auto app = new crs::console::application(argc_test, argv_test, buffer);
-    app->handle();
+    std::stringstream buffer = run_app("car_rental_system -h");
 
     assert_has_text(buffer.str(),
         "Car Rental System - help with the rental process, users and cars management",
@@ -36,11 +63,7 @@ void test_help_when_no_command_expect_see_all_commands_helps()
 
 void test_help_when_register_command_expect_no_other_commands()
 {
-    char* argv_test[] = { (char*)"car_rental_system", (char*)"register", (char*)"-h", };
-    int argc_test = 3;
-    std::stringstream buffer;
-    auto app = new crs::console::application(argc_test, argv_test, buffer);
-    app->handle();
+    std::stringstream buffer = run_app("car_rental_system register -h");
 
     assert_has_text(buffer.str(),
         "Car Rental System - help with the rental process, users and cars management",
@@ -51,13 +74,9 @@ void test_help_when_register_command_expect_no_other_commands()
 
 void test_help_when_command_not_exist_expect_error()
 {
-    char* argv_test[] = { (char*)"car_rental_system", (char*)"no_such_command", (char*)"-h", };
-    int argc_test = 3;
-    std::stringstream buffer;
-    auto app = new crs::console::application(argc_test, argv_test, buffer);
     try
     {
-        app->handle();
+        std::stringstream buffer = run_app("car_rental_system no_such_command -h");
     }
     catch (std::exception& exc)
     {
@@ -71,66 +90,41 @@ void test_help_when_command_not_exist_expect_error()
 
 void test_add_car_when_register_as_admin_expect_car_created()
 {
-    char* opts_register[]
-        { (char*)"car_rental_system", (char*)"register", (char*)"-u", (char*)"u", (char*)"-p", (char*)"p",
-          (char*)"-a", };
-    std::stringstream buffer;
-    (new crs::console::application(7, opts_register, buffer))->handle();
+    auto buffer = run_app("car_rental_system register -u u -p p -a");
 
     assert_has_text(buffer.str(), "Admin with username", __FUNCTION__);
     assert_has_text(buffer.str(), "was created", __FUNCTION__);
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "u", "-p", "p",
-                          "--make", "toyota",
-                          "--model", "x2",
-                          "--year", "2020",
-    };
 
-    std::stringstream buffer_car;
-    (new crs::console::application(12, opts_car_add, buffer_car))->handle();
+    auto buffer_car = run_app(
+        "car_rental_system car:add -u u -p p --make toyota --model x2 --year 2020 --price-per-day 1"
+    );
+
     assert_has_text(buffer_car.str(), "Car", __FUNCTION__);
     assert_has_text(buffer_car.str(), "added", __FUNCTION__);
 }
 
 void add_admin()
 {
-    char* opts_register[]
-        { (char*)"car_rental_system", (char*)"register",
-          (char*)"-u", (char*)"a", (char*)"-p", (char*)"p", (char*)"-a", };
-    std::stringstream buffer;
-    (new crs::console::application(7, opts_register, buffer))->handle();
+    auto buffer_car = run_app("car_rental_system register -u a -p p -a");
 }
 
 void add_customer()
 {
-    char* opts_register[]
-        { (char*)"car_rental_system", (char*)"register",
-          (char*)"-u", (char*)"c", (char*)"-p", (char*)"p" };
-    std::stringstream buffer;
-    (new crs::console::application(6, opts_register, buffer))->handle();
+    auto buffer_car = run_app("car_rental_system register -u c -p p");
 }
 
 void test_calculate_rent_when_car_available_expect_correct_price()
 {
     add_admin();
     // add car
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "a", "-p", "p",
-                          "--make", "toyota",
-                          "--model", "x2",
-                          "--year", "2020",
-                          "--price-per-day", "1"
-    };
+    auto buffer_car = run_app(
+        "car_rental_system car:add -u a -p p --make toyota --model x2 --year 2020 --price-per-day 1"
+    );
 
-    std::stringstream buffer_car;
-    (new crs::console::application(14, opts_car_add, buffer_car))->handle();
     // see price
-    char* opts_calculate[]{ (char*)"car_rental_system", "rent:calculate",
-                            "--start", "01/01/2025",
-                            "--end", "10/01/2025",
-                            "-i", "1"
-    };
-
-    std::stringstream buffer_calc;
-    (new crs::console::application(8, opts_calculate, buffer_calc))->handle();
+    auto buffer_calc = run_app(
+        "car_rental_system rent:calculate --start 01/01/2025 --end 10/01/2025 -i 1"
+    );
     assert_has_text(buffer_calc.str(), ": 10 NZD", __FUNCTION__);
 }
 
@@ -138,24 +132,15 @@ void test_calculate_or_book_when_car_booked_expect_error_when_dates_not_availabl
 {
     add_admin();
     // add car
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "a", "-p", "p",
-                          "--make", "toyota",
-                          "--model", "x2",
-                          "--year", "2020",
-                          "--price-per-day", "1"
-    };
-    std::stringstream buffer_car;
-    (new crs::console::application(14, opts_car_add, buffer_car))->handle();
+    auto buffer_car = run_app(
+        "car_rental_system car:add -u a -p p --make toyota --model x2 --year 2020 --price-per-day 1"
+    );
 
     add_customer();
-    char* opts_calculate[]{ (char*)"car_rental_system", "booking:add",
-                            "--start", "01/01/2025",
-                            "--end", "10/01/2025",
-                            "-i", "1",
-                            "-u", "c", "-p", "p",
-    };
-    std::stringstream buffer_calc;
-    (new crs::console::application(12, opts_calculate, buffer_calc))->handle();
+    // book car
+    auto buffer_calc = run_app(
+        "car_rental_system booking:add --start 01/01/2025 --end 10/01/2025 -i 1 -u c -p p"
+    );
 
     std::list<array<char*, 2>> unavailable_dates{
         array<char*, 2>{ "01/01/2025", "10/01/2025" },
@@ -172,22 +157,17 @@ void test_calculate_or_book_when_car_booked_expect_error_when_dates_not_availabl
 
     // another customer can NOT book those dates
     bool has_expected_exception;
-    std::stringstream buffer;
 
     for (const auto& dates : unavailable_dates)
     {
-        char* opts_calculate[]{ (char*)"car_rental_system", "rent:calculate",
-                                "--start", dates[0],
-                                "--end", dates[1],
-                                "-i", "1"
-        };
         has_expected_exception = false;
-        buffer.clear();
-        buffer.str(std::string());
 
         try
         {
-            (new crs::console::application(8, opts_calculate, buffer))->handle();
+            auto buffer = run_app(
+                "car_rental_system rent:calculate --start " + std::string(dates[0]) +
+                    " --end " + std::string(dates[1]) + " -i 1"
+            );
         }
         catch (const std::exception& e)
         {
@@ -203,19 +183,14 @@ void test_calculate_or_book_when_car_booked_expect_error_when_dates_not_availabl
     }
     for (const auto& dates : unavailable_dates)
     {
-        char* opts_calculate[]{ (char*)"car_rental_system", "booking:add",
-                                "--start", dates[0],
-                                "--end", dates[1],
-                                "-i", "1", "-u", "c", "-p", "p",
-
-        };
         has_expected_exception = false;
-        buffer.clear();
-        buffer.str(std::string());
 
         try
         {
-            (new crs::console::application(12, opts_calculate, buffer))->handle();
+            auto buffer = run_app(
+                "car_rental_system booking:add --start " + std::string(dates[0]) +
+                    " --end " + std::string(dates[1]) + " -i 1 -u c -p p"
+            );
         }
         catch (const std::exception& e)
         {
@@ -239,15 +214,10 @@ void test_calculate_or_book_when_car_booked_expect_error_when_dates_not_availabl
     };
     for (const auto& dates : available_dates)
     {
-        char* opts_calculate[]{ (char*)"car_rental_system", "rent:calculate",
-                                "--start", dates[0],
-                                "--end", dates[1],
-                                "-i", "1"
-        };
-        buffer.clear();
-        buffer.str(std::string());
-
-        (new crs::console::application(8, opts_calculate, buffer))->handle();
+        auto buffer = run_app(
+            "car_rental_system rent:calculate --start " + std::string(dates[0]) +
+                " --end " + std::string(dates[1]) + " -i 1"
+        );
 
         assert_has_text(buffer.str(), "Total payment will be", __FUNCTION__);
     }
@@ -286,29 +256,22 @@ void test_book_car_when_car_available_expect_success_booking()
 
 void test_car_list_by_customer_when_car_created_by_admin_expect_customer_sees_car()
 {
-    char* opts_register[]{ (char*)"car_rental_system", "register", "-u", "u", "-p", "p", "-a", };
-    std::stringstream buffer;
-    (new crs::console::application(7, opts_register, buffer))->handle();
+    add_admin();
 
-    assert_has_text(buffer.str(), "Admin with username", __FUNCTION__);
-    assert_has_text(buffer.str(), "was created", __FUNCTION__);
-    char* make = "toyota";
-    char* model = "x2";
-    char* year = "2020";
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "u", "-p", "p",
-                          "--make", make,
-                          "--model", model,
-                          "--year", year,
-    };
+    std::string make = "toyota";
+    std::string model = "x2";
+    std::string year = "2020";
 
-    std::stringstream buffer_car;
-    (new crs::console::application(12, opts_car_add, buffer_car))->handle();
+    auto buffer_car = run_app(
+        "car_rental_system car:add -u a -p p --price-per-day 1"
+        " --make " + make + " --model " + model + " --year " + year
+    );
     assert_has_text(buffer_car.str(), "Car", __FUNCTION__);
     assert_has_text(buffer_car.str(), "added", __FUNCTION__);
 
-    char* opts_car_list[]{ (char*)"car_rental_system", "car:list", };
-    std::stringstream buffer_list;
-    (new crs::console::application(2, opts_car_list, buffer_list))->handle();
+    auto buffer_list = run_app(
+        "car_rental_system car:list"
+    );
 
     assert_has_text(buffer_list.str(), "Car List", __FUNCTION__);
     assert_has_text(buffer_list.str(), make, __FUNCTION__);
@@ -318,22 +281,18 @@ void test_car_list_by_customer_when_car_created_by_admin_expect_customer_sees_ca
 
 void test_add_car_when_register_as_customer_expect_failed_to_add()
 {
-    char* opts_register[]{ (char*)"car_rental_system", "register", "-u", "u", "-p", "p", };
-    std::stringstream buffer;
-    (new crs::console::application(6, opts_register, buffer))->handle();
+    auto buffer = run_app(
+        "car_rental_system register -u u -p p"
+    );
 
     assert_has_text(buffer.str(), "User with username", __FUNCTION__);
     assert_has_text(buffer.str(), "was created", __FUNCTION__);
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "u", "-p", "p",
-                          "--make", "toyota",
-                          "--model", "x2",
-                          "--year", "2020",
-    };
 
-    std::stringstream buffer_car;
     try
     {
-        (new crs::console::application(12, opts_car_add, buffer_car))->handle();
+        auto buffer_car = run_app(
+            "car_rental_system car:add -u u -p p --make toyota --model x2 --year 2020"
+        );
     }
     catch (const std::exception& exc)
     {
@@ -349,13 +308,27 @@ void test_add_car_when_register_as_customer_expect_failed_to_add()
 
 void test_car_add_when_not_existed_user_expect_error()
 {
-    char* opts_car_add[]{ (char*)"car_rental_system", "car:add", "-u", "no_such_user", "-p", "p" };
-
-    std::stringstream buffer;
-    auto app = new crs::console::application(6, opts_car_add, buffer);
     try
     {
-        app->handle();
+        auto buffer = run_app(
+            "car_rental_system car:add -u no_such_user -p p"
+        );
+    }
+    catch (const std::exception& exc)
+    {
+        assert_equals((std::string)"User not found!", (std::string)exc.what(), __FUNCTION__);
+        return;
+    };
+    show_error_msg("Expected an exception but didn't have any", __FUNCTION__);
+}
+
+void test_car_add_when_not_existed_user_expect_error2()
+{
+    try
+    {
+        auto buffer = run_app(
+            "car_rental_system car:add -u no_such_user -p p"
+        );
     }
     catch (const std::exception& exc)
     {
@@ -387,6 +360,7 @@ int main()
         []() -> void { test_book_car_when_car_available_expect_success_booking(); },
         []() -> void { test_add_car_when_register_as_customer_expect_failed_to_add(); },
         []() -> void { test_car_add_when_not_existed_user_expect_error(); },
+        []() -> void { test_car_add_when_not_existed_user_expect_error2(); },
         []() -> void { test_create_user(); },
     };
     for (auto& test : tests)
