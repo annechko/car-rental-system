23774068c62499c6927910aebbaf75c4ef4ad7aa
Anna B <annnechko@gmail.com>
2024-01-22 12:37:07 +1300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#include "console/application.h"
-#include "core/core_exception.hpp"
-#include "console/text_helper.hpp"
+
 
 int main(int argc, char* argv[])
 {
-    auto app = new crs::console::application(argc, argv, std::cout);
-
-    try
-    {
-        app->handle();
-    }
-    catch (const std::exception& exception)
-    {
-        std::cout << crs::console::text_helper::red(exception.what()) << std::endl;
-        return EXIT_FAILURE;
-    }
 
     return EXIT_SUCCESS;
 }
